af883c68ad612cb4329b1632452402e46dc428be
shivam <gshivam@startxlabs.in>
2023-09-04 17:06:09 +0530

    extension added

--- src/components/Pages/dashboard.tsx
+++ src/components/Pages/dashboard.tsx
@@ -2,17 +2,21 @@ import Dialpad from "components/Keypad/Dialpad";
 import LayoutWrapper from "components/LayoutWrapper";
 import React from "react";
 import classes from "./dashboard.module.scss";
+import ContactList from "components/shared/ContactList";
+import ProfileAndExtension from "components/shared/ProfileAndExtension";
 
 const Dashboard = () => {
   return (
     <LayoutWrapper>
       <section className={classes.dashboard}>
         <div className={classes.contact}>
-          Hello kjfdsjflksdlkf sdjflksdjflsd fsdoifdsf sdlkfsdm,f
-          sdlkjfsdlkflkdsfsfklsdnfsdflknl
+          <ContactList />
         </div>
         <div className={classes.dialpad}>
           <Dialpad />
+        </div>
+        <div className={classes.profileAndExtension}>
+          <ProfileAndExtension />
         </div>
       </section>
     </LayoutWrapper>
