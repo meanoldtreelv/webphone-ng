ae4a13ebd9a291b8e24be0e38fee9564952a3bb9
shivam <gshivam@startxlabs.in>
2023-09-12 08:54:12 +0530

    add contact card done

--- src/components/Pages/contact.tsx
+++ src/components/Pages/contact.tsx
@@ -1,16 +1,23 @@
 import NoContact from "components/Contact/NoContact";
+import NoContactSelected from "components/Contact/NoContactSelected";
 import LayoutWrapper from "components/LayoutWrapper";
 import ContactList from "components/shared/ContactList";
 import React from "react";
+import classes from "./contact.module.scss";
+import AddContact from "components/Contact/AddContact";
 
 const Contact = () => {
 	return (
-		<LayoutWrapper>
-			{/* <NoContact /> */}
-			<section>
-				<ContactList />
-			</section>
-		</LayoutWrapper>
+		<div style={{ position: "relative", width: "100%", height: "100vh" }}>
+			<LayoutWrapper>
+				{/* <NoContact /> */}
+				<section className={`flex ${classes.contact_container}`}>
+					<ContactList />
+					<NoContactSelected />
+				</section>
+			</LayoutWrapper>
+			<AddContact />
+		</div>
 	);
 };
 
