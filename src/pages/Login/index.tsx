3bd7ffdd94d4a8ee96275e8cc59ec1f20b953eb2
leulmesfin3 <leul.mesfin.3@gmail.com>
2023-11-06 20:21:55 +0300

    login issue

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -43,7 +43,7 @@ const Login = () => {
 	}
 	const [form, setForm] = useState<FormData>({ extension: "", secret: "", server: "zraytechnoloDoobh.ringplan.com" });
 	const loginWithExtension = () => {
-		sip.logout()
+		// sip.logout()
 		store.dispatch({ type: "sip/extAuth", payload: true });
 		setCookie("extAuth", "true");
 		form?.extension === ""
