11310519bd32f7ee73517649b643cf14b17eba42
eyosiyas_startxlabs <eyosiyas@startxlabs.com>
2023-12-08 07:36:21 +0300

    Session & cookie issue fix

--- src/middleware/jwtTokenRefresher.ts
+++ src/middleware/jwtTokenRefresher.ts
@@ -10,7 +10,7 @@ export const jwtTokenRefresher =
 	(next: any) =>
 	async (action: any) => {
 		if (action && isRejectedWithValue(action)) {
-			if (localStorage.getItem('extAuth') !== "true") {
+			if (localStorage.getItem("extAuth") !== "true") {
 				if (action?.payload?.response?.status === 401) {
 					await axios
 						.post("https://b2clogin.ringplan.com/refresh-token", {
@@ -18,8 +18,16 @@ export const jwtTokenRefresher =
 							refresh_token: getCookie("refresh_token"),
 						})
 						.then((resp) => {
-							setCookie("id_token", resp?.data?.id_token);
-							setCookie("refresh_token", resp?.data?.refresh_token);
+							// setCookie("id_token", resp?.data?.id_token);
+							Cookies.set("id_token", resp?.data?.id_token, {
+								expires: 7,
+								path: ".ringplan.com",
+							});
+							Cookies.set("refresh_token", resp?.data?.id_token, {
+								expires: 7,
+								path: ".ringplan.com",
+							});
+							// setCookie("refresh_token", resp?.data?.refresh_token);
 							window.location.reload();
 						})
 						.catch((e) => {
