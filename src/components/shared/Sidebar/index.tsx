649f1700ce05ce04fbfb02cf8fb935516f3e9c78
shivam <gshivam@startxlabs.in>
2023-12-21 23:46:20 +0530

    sidebar ui issue fix

--- src/components/shared/Sidebar/index.tsx
+++ src/components/shared/Sidebar/index.tsx
@@ -4,10 +4,8 @@ import styles from "./Sidebar.module.scss";
 import routePaths from "./../../../constants/routes";
 import SettingsIcon from "components/UI/Icons/Sidebar/Settings";
 import VoicemailIcon from "components/UI/Icons/Sidebar/Voicemail";
-import FaxIcon from "components/UI/Icons/Sidebar/Fax";
 import RecentsIcon from "components/UI/Icons/Sidebar/Recents";
 import ChatIcon from "components/UI/Icons/Sidebar/Chat";
-import UserGroupIcon from "components/UI/Icons/Sidebar/UserGroup";
 import ContactIcon from "components/UI/Icons/Sidebar/Contact";
 import KeypadIcon from "components/UI/Icons/Sidebar/Keypad";
 import SidecarIcon from "components/UI/Icons/Sidebar/Sidecar";
@@ -16,9 +14,10 @@ import { useDispatch, useSelector } from "react-redux";
 import Logo from "components/UI/Logo";
 import { ClipLoader } from "react-spinners";
 import { loader } from "redux/common/commonSelectors";
-import { getCookie } from "typescript-cookie";
 import { unreadMessageCount } from "redux/chat/chatSelectors";
 import { setUnreadMessageCount } from "redux/chat/chatSlice";
+import FaxIcon from "components/UI/Icons/Sidebar/Fax";
+import UserGroupIcon from "components/UI/Icons/Sidebar/UserGroup";
 
 interface ISidebarLinks {
 	path: string;
@@ -26,67 +25,77 @@ interface ISidebarLinks {
 	unread: number;
 	name: string;
 }
+
 const Sidebar = () => {
 	const dispatch = useDispatch();
 	const location = useLocation();
+
 	const unreadCount = useSelector(unreadMessageCount);
-	const [isCollapsed, setIsCollapsed] = useState(false);
 	const mainLoader = useSelector(loader);
-	const [tabActive, setTabActive] = useState("1");
-	const [tabHovered, setTabHovered] = useState("1");
-
-	const [unreadMessage, setUnreadMessage] = useState(true);
-
 	const { extAuth } = useSelector((state: any) => state.sip);
-	// the above two functions, they need to be removed
-	// use sass
-	const activeTabStyle = {
-		background: "var(--background-active, rgba(25, 138, 240, 0.1))",
-		color: "var(--text-link, #1480e1)",
-	};
 
-	const hoveredTabStyle = {
-		background: "var(--background-active, rgba(25, 138, 240, 0.1))",
-		color: "var(--text-link, #1480e1)",
-	};
-
-	// const toggleCollapsed = () => {
-	// 	setIsCollapsed(!isCollapsed);
-	// };
-
-	// const sidebarTopLinks: ISidebarLinks[] = extAuth
-
-	console.log(unreadCount, "unreadCount");
+	const [isCollapsed, setIsCollapsed] = useState(false);
+	const [tabHovered, setTabHovered] = useState("");
 
 	const sidebarTopLinks: ISidebarLinks[] = extAuth
 		? [
 				{
 					path: routePaths.DASHBOARD.ROUTE,
-					icon: <KeypadIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<KeypadIcon
+							color={`${
+								tabHovered === "Keypad" || location.pathname === "/dashboard" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Keypad",
 					unread: 0,
 				},
 				{
 					path: routePaths.CONTACT.ROUTE,
-					icon: <ContactIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<ContactIcon
+							color={`${
+								tabHovered === "Contacts" || location.pathname === "/contact" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Contacts",
 					unread: 0,
 				},
 				{
 					path: routePaths.CHAT.ROUTE,
-					icon: <ChatIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<ChatIcon
+							color={`${
+								tabHovered === "Texting" || location.pathname === "/texting" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Texting",
 					unread: unreadCount,
 				},
 				{
 					path: routePaths.CALL_HISTORY.ROUTE,
-					icon: <RecentsIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<RecentsIcon
+							color={`${
+								tabHovered === "Recent" || location.pathname === "/call-history" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Recent",
 					unread: 0,
 				},
 				{
 					path: routePaths.VOICEMAIL.ROUTE,
-					icon: <VoicemailIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<VoicemailIcon
+							color={`${
+								tabHovered === "Voicemail" || location.pathname === "/voicemail" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Voicemail",
 					unread: 0,
 				},
@@ -94,52 +103,84 @@ const Sidebar = () => {
 		: [
 				{
 					path: routePaths.DASHBOARD.ROUTE,
-					icon: <KeypadIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<KeypadIcon
+							color={`${
+								tabHovered === "Keypad" || location.pathname === "/dashboard" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Keypad",
 					unread: 0,
 				},
 				{
 					path: routePaths.CONTACT.ROUTE,
-					icon: <ContactIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<ContactIcon
+							color={`${
+								tabHovered === "Contacts" || location.pathname === "/contact" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Contacts",
 					unread: 0,
 				},
 				{
 					path: routePaths.CHAT.ROUTE,
-					icon: <ChatIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<ChatIcon
+							color={`${
+								tabHovered === "Texting" || location.pathname === "/texting" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Texting",
 					unread: unreadCount,
 				},
+
+				{
+					path: routePaths.CALL_HISTORY.ROUTE,
+					icon: (
+						<RecentsIcon
+							color={`${
+								tabHovered === "Recent" || location.pathname === "/call-history" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
+					name: "Recent",
+					unread: 0,
+				},
+
+				{
+					path: routePaths.VOICEMAIL.ROUTE,
+					icon: (
+						<VoicemailIcon
+							color={`${
+								tabHovered === "Voicemail" || location.pathname === "/voicemail" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
+					name: "Voicemail",
+					unread: 0,
+				},
 				// {
 				// 	path: routePaths.CONFERENCE.GROUPS.ROUTE,
-				// 	icon: <UserGroupIcon tabActive={tabActive} tabHovered={tabHovered} />,
+				// 	icon: (
+				// 		<UserGroupIcon
+				// 			color={`${tabHovered === "Conference" || location.pathname === "/" ? "primary-default" : "icon-primary"}`}
+				// 		/>
+				// 	),
 				// 	name: "Conference",
 				// 	unread: 0,
 				// },
 				// {
 				// 	path: routePaths.CONFERENCE.ROUTE,
-				// 	icon: <ChatIcon tabActive={tabActive} tabHovered={tabHovered} />,
-				// 	name: "Texting",
-				// 	unread: 0,
-				// },
-				{
-					path: routePaths.CALL_HISTORY.ROUTE,
-					icon: <RecentsIcon tabActive={tabActive} tabHovered={tabHovered} />,
-					name: "Recent",
-					unread: 0,
-				},
-				// {
-				// 	path: routePaths.CONFERENCE.ROUTE,
-				// 	icon: <FaxIcon tabActive={tabActive} tabHovered={tabHovered} />,
+				// 	icon: (
+				// 		<FaxIcon color={`${tabHovered === "Fax" || location.pathname === "/" ? "primary-default" : "icon-primary"}`} />
+				// 	),
 				// 	name: "Fax",
 				// 	unread: 0,
 				// },
-				{
-					path: routePaths.VOICEMAIL.ROUTE,
-					icon: <VoicemailIcon tabActive={tabActive} tabHovered={tabHovered} />,
-					name: "Voicemail",
-					unread: 0,
-				},
 		  ];
 
 	const sidebarBtmLinks: ISidebarLinks[] = extAuth
@@ -147,7 +188,13 @@ const Sidebar = () => {
 				{ path: routePaths.MEET.ROUTE, icon: <MeetIcon />, name: "RingPlan Meet", unread: 0 },
 				{
 					path: routePaths.SETTINGS.ROUTE,
-					icon: <SettingsIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<SettingsIcon
+							color={`${
+								tabHovered === "Settings" || location.pathname === "/settings" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Settings",
 					unread: 0,
 				},
@@ -157,7 +204,13 @@ const Sidebar = () => {
 				{ path: routePaths.MEET.ROUTE, icon: <MeetIcon />, name: "RingPlan Meet", unread: 0 },
 				{
 					path: routePaths.SETTINGS.ROUTE,
-					icon: <SettingsIcon tabActive={tabActive} tabHovered={tabHovered} />,
+					icon: (
+						<SettingsIcon
+							color={`${
+								tabHovered === "Settings" || location.pathname === "/settings" ? "primary-default" : "icon-primary"
+							}`}
+						/>
+					),
 					name: "Settings",
 					unread: 0,
 				},
@@ -193,14 +246,17 @@ const Sidebar = () => {
 								className={({ isActive }: { isActive: boolean }) =>
 									[styles.sidebar_tab, isActive ? styles.active_tab : ""].join(" ")
 								}
-								key={link.name}>
-								<span
-									className={`${!isCollapsed && link.unread > 0 ? styles.sidebar_icon : ""}`}
-									onClick={() => {
-										unreadHandler(link?.path);
-									}}>
-									{link.icon}
-								</span>
+								key={link.name}
+								onClick={() => {
+									unreadHandler(link?.path);
+								}}
+								onMouseOver={() => {
+									setTabHovered(link?.name);
+								}}
+								onMouseOut={() => {
+									setTabHovered("");
+								}}>
+								<span className={`${!isCollapsed && link.unread > 0 ? styles.sidebar_icon : ""}`}>{link.icon}</span>
 								{isCollapsed && (
 									<span className={`${styles.sidebar_tabExpanded}`}>
 										<span>{link.name}</span>
